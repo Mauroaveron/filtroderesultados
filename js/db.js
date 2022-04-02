c2071545245031aa938cbceec00ff689036643af
mauroaveron <mauroaveron@gmail.com>
2022-04-02 18:14:50 -0300

    first commit

---
+++ js/db.js
@@ -0,0 +1,174 @@
+const autos = [
+	{
+		marca: 'BMW',
+		modelo: 'Serie 3',
+		year: 2022,
+		precio: 30000,
+		puertas: 4,
+		color: 'Blanco',
+		transmision: 'automatico'
+	},
+	{ 
+        marca: 'Audi', 
+        modelo: 'A4', 
+        year: 2022, 
+        precio: 40000, 
+        puertas: 4, 
+        color: 'Negro', 
+        transmision: 'automatico' 
+    },
+	{
+		marca: 'Ford',
+		modelo: 'Mustang',
+		year: 2017,
+		precio: 20000,
+		puertas: 2,
+		color: 'Blanco',
+		transmision: 'automatico'
+	},
+	{ 
+        marca: 'Audi', 
+        modelo: 'A6', 
+        year: 2022, 
+        precio: 35000, 
+        puertas: 4, 
+        color: 'Negro', 
+        transmision: 'automatico' 
+    },
+	{
+		marca: 'BMW',
+		modelo: 'Serie 5',
+		year: 2018,
+		precio: 70000,
+		puertas: 4,
+		color: 'Rojo',
+		transmision: 'automatico'
+	},
+	{
+		marca: 'Mercedes Benz',
+		modelo: 'Clase C',
+		year: 2017,
+		precio: 25000,
+		puertas: 4,
+		color: 'Blanco',
+		transmision: 'automatico'
+	},
+	{
+		marca: 'Chevrolet',
+		modelo: 'Camaro',
+		year: 2020,
+		precio: 60000,
+		puertas: 2,
+		color: 'Rojo',
+		transmision: 'manual'
+	},
+	{ 
+        marca: 'Ford', 
+        modelo: 'Mustang', 
+        year: 2021, 
+        precio: 80000, 
+        puertas: 2, 
+        color: 'Rojo', 
+        transmision: 'manual' 
+    },
+	{
+		marca: 'Dodge',
+		modelo: 'Challenger',
+		year: 2022,
+		precio: 40000,
+		puertas: 4,
+		color: 'Blanco',
+		transmision: 'automatico'
+	},
+	{ 
+        marca: 'Audi', 
+        modelo: 'A3', 
+        year: 2019, 
+        precio: 55000, 
+        puertas: 2, 
+        color: 'Negro', 
+        transmision: 
+        'manual' 
+    },
+	{
+		marca: 'Dodge',
+		modelo: 'Challenger',
+		year: 2022,
+		precio: 25000,
+		puertas: 2,
+		color: 'Rojo',
+		transmision: 'manual'
+	},
+	{
+		marca: 'Mercedes Benz',
+		modelo: 'Clase C',
+		year: 2020,
+		precio: 45000,
+		puertas: 4,
+		color: 'Azul',
+		transmision: 'automatico'
+	},
+	{
+		marca: 'BMW',
+		modelo: 'Serie 5',
+		year: 2021,
+		precio: 90000,
+		puertas: 4,
+		color: 'Blanco',
+		transmision: 'automatico'
+	},
+	{ 
+        marca: 'Ford', 
+        modelo: 'Mustang', 
+        year: 2019, 
+        precio: 60000, 
+        puertas: 2, 
+        color: 'Negro', 
+        transmision: 'manual' 
+    },
+	{
+		marca: 'Dodge',
+		modelo: 'Challenger',
+		year: 2017,
+		precio: 35000,
+		puertas: 2,
+		color: 'Azul',
+		transmision: 'automatico'
+	},
+	{
+		marca: 'BMW',
+		modelo: 'Serie 3',
+		year: 2020,
+		precio: 50000,
+		puertas: 4,
+		color: 'Blanco',
+		transmision: 'automatico'
+	},
+	{
+		marca: 'BMW',
+		modelo: 'Serie 5',
+		year: 2019,
+		precio: 80000,
+		puertas: 4,
+		color: 'Negro',
+		transmision: 'automatico'
+	},
+	{
+		marca: 'Mercedes Benz',
+		modelo: 'Clase C',
+		year: 2020,
+		precio: 40000,
+		puertas: 4,
+		color: 'Blanco',
+		transmision: 'automatico'
+	},
+	{ 
+        marca: 'Audi', 
+        modelo: 'A4', 
+        year: 2018, 
+        precio: 30000, 
+        puertas: 4, 
+        color: 'Azul', 
+        transmision: 'automatico' 
+    }
+];
